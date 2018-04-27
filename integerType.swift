f22a53652463ad714e21386571b06f4fab93b4e3
Yusif <kerimov.y007@gmail.com>
2018-04-27 23:20:45 +0400

    Update integerType.swift

--- integerType.swift
+++ integerType.swift
@@ -1,7 +1,11 @@
 // integer(Int , UInt)  type
 // UInt8,UInt16,UInt32,UInt64 , Int8,Int16,Int32,Int64
-var x: UInt16
+var x: UInt16 //type annotation 
 x = UInt16.max
 print(x)
-typealias  typeInt=Int
+typealias  typeInt=Int // type alias
 print(typeInt.min)
+
+var b,c: Int 
+b = 23 ;  c = 45
+print(b*c)
