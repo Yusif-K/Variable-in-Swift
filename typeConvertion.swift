56bfea05fbadd8dc60d5dc49a64e7dcf0a84f3ca
Yusif <kerimov.y007@gmail.com>
2018-04-28 01:16:35 +0400

    Update typeConvertion.swift

--- typeConvertion.swift
+++ typeConvertion.swift
@@ -1,1 +1,11 @@
+///type convertation
+var vint8: Int8 = 34 
+var vint16: Int16 = 434 
+// var vint = vint8+ vint16  // error
+var vint = Int16(vint8) + vint16  
+print(vint)
 
+// var dbl = 23423.443 + vint //error
+var dbl = 23423.443 + Double(vint)
+
+print(dbl,",\(Int(34.5) + 45)")
